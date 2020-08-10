84109bd8d3c439852f07a96ebc8cedc861c1597a
epikem <epikem1@gmail.com>
2020-08-10 14:11:30 +0000

    Add scan route

--- server/routes/scan.js
+++ server/routes/scan.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const models = require('../models');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('this is scan route');
+  console.log(req.session);
+  // console.log(req.session.table);
+
+  res.json(req.session);
+})
+
+router.post('/', async function(req, res, next) {
+  console.log(req.session);
+  console.log(req.session.user.mac);
+  if(!req.session.user.mac){
+    console.debug('please login first');
+    res.redirect('/users/login');
+    return;
+  }
+  
+  const created = await models.Scan
+    .findOrCreate({
+      where: {
+        my_mac:req.session.user.mac,
+        scan_mac:req.body.scan_mac,
+      }
+    })
+
+  res.sendStatus(201);
+})
+
 });
 
 module.exports = router;
