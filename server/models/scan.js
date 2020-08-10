6e75382e0ca385001c49a9bec627d70508abe603
epikem <epikem1@gmail.com>
2020-08-10 07:58:17 +0000

    feat(server): add scan model

---
+++ server/models/scan.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = function (sequelize, DataTypes) {
+  const scan = sequelize.define('Scan', {
+    my_mac: { field: 'my_mac', type: DataTypes.STRING(50), allowNull: false, primaryKey:false },
+    scan_mac: { field: 'scan_mac', type: DataTypes.STRING(50), allowNull: false },
+    scan_time: { field: 'scan_time', type: DataTypes.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+  }, {
+    underscored: true,
+    indexes: [
+      {unique:false,
+      fields:['my_mac']}
+    ],
+
+    tableName: 'scan'
+  });
+
+  return scan;
+};
