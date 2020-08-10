d859dd5c376e1ebaa86e96a895154686582c9948
epikem <epikem1@gmail.com>
2020-08-10 09:09:57 +0000

    Update user login route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,44 @@
 var express = require('express');
 var router = express.Router();
-const {User} = require('../models');
+const models = require('../models');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', async function(req, res, next) {
   console.debug('login');
+  if(!req.body.google_id){
+    console.error('google id is null');
+    res.sendStatus(400);
+  }
 
-  console.log(User);
+  const preUser = await models.User.findOne({
+    where:{
+      mac: req.body.mac
+    }
+  })
 
-  User
-  .findOrCreate({where: {googleID: req.body.googleID,
+  if(preUser){
+    console.info('mac already exists. checking whether this is a new user');
+    if(!preUser.google_id){
+      console.info('this is a new user');
+      await preUser.update({
+        google_id: req.body.google_id
+      })
+
+      if(!preUser.google_id){
+        console.error('what?? this is no way.')
+      }
+      res.sendStatus(200);
+      // TODO: check that user model is fully configured 
+    } else {
+      console.debug('google id is also exists. inserting new record');
+    }
+  }
+ 
+  models.User
+  .findOrCreate({where: {google_id: req.body.google_id,
   mac: req.body.mac}})
   .then(([user, created]) => {
     console.log(user.get({
@@ -21,14 +47,18 @@ router.post('/login', function(req, res, next) {
     if(created==false){
       // user already exists. doesn't matter.
     }
+
     req.session.user=user;
+
     console.log(created);
-    res.send(200); 
+    res.sendStatus(200);
   }).catch(function(error){
     
     console.error(error);
     res.sendStatus(400);
   })
+})
+
     
 })
 
