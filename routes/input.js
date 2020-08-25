75e46036da46fdcb8ca969e6320812e666d4e69e
김태양 <xodid0936@gmail.com>
2020-08-25 23:50:30 +0900

    Modify code align

--- routes/input.js
+++ routes/input.js
@@ -1,35 +1,39 @@
-var express    = require('express');
-var db         = require('../dbconfig');
+var express = require('express');
+var db = require('../dbconfig');
 var router = express.Router();
 
-router.post('/', function(req,res){
-  var my_key = req.query.my_key;
+router.post('/', function(req, res) {
+  var my_key = req.body[0].my_key;
   var scan = req.body;
   var data = req.body;
-  var length = req.body.length;
+  var length = scan.length;
+  console.log(req.body);
   console.log(length);
   var insert_sql = "insert into scan (my_key, scan_key, scan_time) values ";
 
   var scan_key, scan_time;
 
-  for (var i = 0; i < length; ++i)
-  {
+  for (var i = 0; i < length; ++i) {
     scan_key = data[i].scan_key;
     scan_time = data[i].scan_time;
 
-    insert_sql += "('" + my_key + "', '"  + scan_key + "', '" + scan_time + "'),";
+    insert_sql += "('" + my_key + "', '" + scan_key + "', '" + scan_time + "'),";
   }
 
-    insert_sql = insert_sql.slice(0,-1);
+  insert_sql = insert_sql.slice(0, -1);
   console.log(insert_sql);
-    db.mysql.query(insert_sql,function(err,result){
-      if(err){
-        console.log(err);
-        res.json({'res' : '-1'});
-      }
-      console.log('input success!');
-      res.json({'res' : '0'});
+  db.mysql.query(insert_sql, function(err, result) {
+    if (err) {
+      console.log(err);
+      res.json({
+        'res': '-1'
+      });
+    }
+    console.log('input success!');
+    res.json({
+      'res': '0'
     });
+  });
 
 });
 
--- routes/testlogin.js
+++ routes/testlogin.js
@@ -1,62 +1,72 @@
-var express    = require('express');
-var db         = require('../dbconfig');
-var uuid4      = require('uuid4');
+var express = require('express');
+var db = require('../dbconfig');
+var uuid4 = require('uuid4');
 var router = express.Router();
 
-router.post('/', function(req,res){
+router.post('/', function(req, res) {
   console.log('someggggggggggggggggggggggggggggggggggg');
   console.log(req.body);
   var google_id = req.body.google_id;
   var token = req.body.token;
-  var my_key = 'data123';
+  var my_key;
   var login_sql = 'select * From testmembers where google_id = ?';
 
-  db.mysql.query(login_sql, google_id, function(err,rows,fields){
-    if(err){
+  db.mysql.query(login_sql, google_id, function(err, rows, fields) {
+    if (err) {
       console.log('137 err :' + err);
-      res.json({'res':'1','state':'-1'});
+      res.json({
+        'res': '-1'
+      });
     } else {
       console.log(rows);
-      if(rows[0] == undefined){
+      if (rows[0] == undefined) {
         console.log(rows[0]);
-
 
         var insert_sql = 'insert into testmembers (google_id,token,my_key,lastest_use) values(?,?,?,?)';
         var now_time = new Date();
+        my_key = 'data123';
 
-        var param = [google_id,token,my_key,now_time];
-        db.mysql.query(insert_sql,param,function(err,result){
-          if(err){
+        var param = [google_id, token, my_key, now_time];
+        db.mysql.query(insert_sql, param, function(err, result) {
+          if (err) {
             console.log('150 err :' + err);
-            res.json({'res':'-1'});
-          }else {
+            res.json({
+              'res': '-1'
+            });
+          } else {
             console.log('input success!');
           }
         });
         console.log("sign up success");
-        res.json({'res': my_key});
+        res.json({
+          'res': my_key
+        });
       } else {
         console.log('login success');
         console.log(rows[0]['google_id'] + ' now state is : ' + rows[0]['state']);
-        var update_sql = 'update members set lastest_use = (?) where google_id = (?)';
+        var update_sql = 'update testmembers set lastest_use = (?) where google_id = (?)';
         var now_time = new Date();
 
-        var param = [now_time,google_id];
-
-        db.mysql.query(update_sql,param,function(err,result){
-          if(err){
+        var param = [now_time, google_id];
+        my_key = rows[0]['my_key'];
+        db.mysql.query(update_sql, param, function(err, result) {
+          if (err) {
             console.log('178 err : ' + err);
-            res.json({'res':'-1'});
-          }else{
+            res.json({
+              'res': '-1'
+            });
+          } else {
             console.log('update lastest use time!');
           }
         });
 
         //req.session.displayname = my_mac;
 
-        var re = [rows[0]['state'],'0'];
-        res.json({'res' : my_key});
-      //  res.render('input_data');
+        var re = [rows[0]['state'], '0'];
+        res.json({
+          'res': my_key
+        });
+        //  res.render('input_data');
       }
     }
   });
