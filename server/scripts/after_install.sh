e375fd241286a07289b68e207cd4a50e9942f9b4
epikem <epikem1@gmail.com>
2020-09-29 11:04:25 +0900

    Update deploy scripts - clean up script

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -10,27 +10,23 @@ APPPATH=/home/ubuntu/server
 /bin/echo "$(date '+%Y-%m-%d'): ** After Install Hook Started **" >> $LOG
 /bin/echo "$(date '+%Y-%m-%d'): Changing owner and group of application... " >> $LOG
 
-# verify that the application directory has the correct owner/group
-# /usr/bin/sudo /bin/chown -R $USER:$USER $APP
-
-# touch ~/.bashrc
-
-# ls $APPPATH >> $LOG 2>&1
-
-# systemctl enable dahda
-
 cd $APPPATH
 
-source $APPPATH/scripts/bash_profile.sh >> $LOG 2>&1
-. $APPPATH/scripts/bash_profile.sh >> $LOG 2>&1
-cat $APPPATH/scripts/bash_profile.sh | /bin/bash >> $LOG 2>&1
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash >> $LOG 2>&1
+
+pwd >> $LOG 2>&1
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts >> $LOG 2>&1
+nvm use --lts >> $LOG 2>&1
+
+npm install forever -g >> $LOG 2>&1
 
 npm install >> $LOG 2>&1
 
 echo -e "NPM Install Done" >> $LOG 2>&1
-
-# pwd >> $LOG 2>&1
-# nvm install --lts
-# nvm use --lts
 
 /bin/echo "$(date '+%Y-%m-%d %X'): ** After Install Hook Completed **" >> $LOG
--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -10,31 +10,8 @@ PIDF=$APP/$APPNAME.pid
 
 /bin/echo "$(date '+%Y-%m-%d %X'): ** Before Install Hook Started **" >> $LOG 2>&1
 /bin/echo "home: $HOME" >> $LOG 2>&1
-/bin/echo "VERSION 0.7-test" >> $LOG 2>&1
-
+/bin/echo "VERSION 0.8-test" >> $LOG 2>&1
 
 # Do some actions before the installation
-cd $APPPATH
-
-source $APPPATH/scripts/bash_profile.sh >> $LOG 2>&1
-. $APPPATH/scripts/bash_profile.sh >> $LOG 2>&1
-cat $APPPATH/scripts/bash_profile.sh | /bin/bash >> $LOG 2>&1
-
-pwd >> $LOG 2>&1
-ls >> $LOG 2>&1
-ls $APPPATH >> $LOG 2>&1
-
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash >> $LOG 2>&1
-
-pwd >> $LOG 2>&1
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install --lts >> $LOG 2>&1
-nvm use --lts >> $LOG 2>&1
-
-npm install forever -g >> $LOG 2>&1
 
 /bin/echo "$(date '+%Y-%m-%d %X'): ** Before Install Hook Completed **" >> $LOG 2>&1
