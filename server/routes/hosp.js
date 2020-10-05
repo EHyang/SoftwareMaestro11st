b927aaf6d575e0c07abfc463606ee57fe25e38e0
김태양 <xodid0936@gmail.com>
2020-10-05 20:06:09 +0900

    Substitute API to Crawling

--- server/routes/hosp.js
+++ server/routes/hosp.js
@@ -3,141 +3,124 @@ var request = require('request');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 
+var axios = require('axios');
+var cheerio =require('cheerio');
+
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  var hospName = [];
+router.get('/', async function(req, res) {
+  let all = "";
+  let id = 1;
 
+  const getHtml = async number => {
+    let ulList = "";
+    console.log(number + "번째 함수 응답");
 
-  var url = 'http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList';
-  var queryParams = '?' + encodeURIComponent('ServiceKey') + '=ixcveqay%2F7Tr%2Fa5YB3dW3zK2O64wxILcNEyCL8l9vFfVVBpipZv99TJJ4XaVolkdhPs%2BzfBxY0bs3Gcbt668CA%3D%3D'; /* Service Key*/
-  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
-  queryParams += '&' + encodeURIComponent('spclAdmTyCd') + '=' + encodeURIComponent('99');
-
-  function get_d() {
-    return new Promise(function(resolve, reject) {
-      console.log("1");
-      // request({
-      //   url: url + queryParams,
-      //   method: 'GET'
-      // }, function(err, response, body) {
-      //   if (body) {
-      //     parser.parseString(body, function(err, result) {
-      //       console.log("여까진 옴");
-      //       var data = result.response.body[0].items[0].item;
-      //       //console.log("이거 되따");
-      //       var length = data.length;
-      //       //console.log(data);
-      //       var sidoNm, sgguNm, yadmNm, telno;
-      //       for (var i = 0; i < length; ++i) {
-      //         var spclAdmTyCd = data[i].spclAdmTyCd;
-      //         if (spclAdmTyCd == '99') {
-      //           //console.log(data[i].yadmNm);
-      //           hospName.push(data[i].yadmNm);
-      //         }
-      //       }
-      //     }); // parser --end
-      //     resolve(hospName);
-      //   }
-      //   reject(new Error("zzzz"));
-      // });
-
-      hospName = ['강남구보건소','삼성서울병원','연세대학교의과대학 강남세브란스병원','강동경희대학교의대병원','강동구보건소'];
-      resolve(hospName);
-
-    });
-  }
-
-
-
-  //console.log("요기이이");
-  get_d().then(function(result) {
-    console.log("2");
-    var to = [];
-
-  function kakaoGet() {
-    return new Promise(function(resolve, reject){
-      console.log("3");
-          var total = [];
-        //   for (var i = 0; i < result.length; i++) {
-        //       console.log("여기");
-        //       var url = 'https://dapi.kakao.com/v2/local/search/keyword.json';
-        //       var queryParams = '?' + encodeURIComponent('page') + '=' + encodeURIComponent('1');
-        //       queryParams += '&' + encodeURIComponent('size') + '=' + encodeURIComponent('1');
-        //       queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent(result[i]);
-        // //console.log("여긴지나나");
-        //
-        //       request({
-        //         url: url + queryParams,
-        //         method: 'GET',
-        //         headers: {
-        //           'Authorization': 'KakaoAK fe7467539d84c3bb76f33d417a5399a4'
-        //         }
-        //       }, function(err, response, body) {
-        //         var js = JSON.parse(body);
-        //         var data = js['documents'];
-        //         var tt = [];
-        //         console.log(data[0]['place_name']);
-        //         console.log(data[0]['x']);
-        //         console.log(data[0]['y']);
-        //         tt.push(data[0]['place_name']);
-        //         tt.push(data[0]['x']);
-        //         tt.push(data[0]['y']);
-        //         total.push(tt);
-        //         //console.log(total);
-        //       }); //request---end
-        //   }
-        for(var i = 0; i<3; i++)
-        {
-          total.push(i);
+    await axios.get(`https://www.mohw.go.kr/react/ncov/selclinic04ls.jsp?page=${number}`).then(html => {
+      const $ = cheerio.load(html.data);
+      const $bodyList = $("table.co_tb_base tbody.tb_center").children("tr");
+      $bodyList.each(function(i, elem) {
+        if ($(this).find('td.name strong').text()) {
+          ulList += $(this).find('td.name strong').text() + '<br>';
+        } else {
+          console.log("ㄴㄴ");
         }
-      resolve(total);
+      });
+      all += ulList;
+      if (number === 30) {
+        res.send(all);
+      }
     });
+  };
+
+  for (var i = 1; i <= 30; i++) {
+    console.log(i + "번째 함수 호출");
+    await getHtml(i);
   }
 
-  kakaoGet().then(function(re){
-    console.log("요긴뎅...");
-    console.log(re);
-  });
+  // function kakaoGet() {
+  //   return new Promise(async function(resolve, reject) {
+  //     console.log("3");
+  //     var total = [];
+  //     let idx = 0;
+  //     var hospName = [
+  //       '강남구보건소*(검체채취 가능)'];
+  //     hospName.forEach(i => {
+  //       console.log("여기" + i);
+  //       ++idx;
+  //       var url = 'https://dapi.kakao.com/v2/local/search/keyword.json';
+  //       var queryParams = '?' + encodeURIComponent('page') + '=' + encodeURIComponent('1');
+  //       queryParams += '&' + encodeURIComponent('size') + '=' + encodeURIComponent('1');
+  //       queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent(i);
+  //       //console.log("여긴지나나");
+  //
+  //       request({
+  //         url: url + queryParams,
+  //         method: 'GET',
+  //         headers: {
+  //           'Authorization': 'KakaoAK fe7467539d84c3bb76f33d417a5399a4'
+  //         }
+  //       }, function(err, response, body) {
+  //         console.log("왜안댕..." + idx);
+  //         var js = JSON.parse(body);
+  //         var data = js['documents'];
+  //         var tt = [];
+  //         console.log(data[0]['place_name']);
+  //         console.log(data[0]['x']);
+  //         console.log(data[0]['y']);
+  //         //tt.push(data[0]['place_name']);
+  //         //tt.push(data[0]['x']);
+  //         //tt.push(data[0]['y']);
+  //         //total.push(tt);
+  //         //console.log(total);
+  //       }); //request---end
+  //     }); //forEach 끝임
+  //     resolve(total);
+  //   });
+  // }
+  //
+  // kakaoGet().then(function(re) {
+  //   console.log("요긴뎅...");
+  //   console.log(re);
+  // });
 
 
 
 
 
-//     for (var i = 0; i < result.length; i++) {
-//       console.log("여기");
-//       var url = 'https://dapi.kakao.com/v2/local/search/keyword.json';
-//       var queryParams = '?' + encodeURIComponent('page') + '=' + encodeURIComponent('1');
-//       queryParams += '&' + encodeURIComponent('size') + '=' + encodeURIComponent('1');
-//       queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent(result[i]);
-// //console.log("여긴지나나");
-//       request({
-//         url: url + queryParams,
-//         method: 'GET',
-//         headers: {
-//           'Authorization': 'KakaoAK fe7467539d84c3bb76f33d417a5399a4'
-//         }
-//       }, function(err, response, body) {
-//         var js = JSON.parse(body);
-//         var data = js['documents'];
-//         var tt = [];
-//         console.log(data[0]['place_name']);
-//         console.log(data[0]['x']);
-//         console.log(data[0]['y']);
-//         tt.push(data[0]['place_name']);
-//         tt.push(data[0]['x']);
-//         tt.push(data[0]['y']);
-//         total.push(tt);
-//         console.log(total);
-//       }); //request---end
-//     }
-    //res.send(total)
+  //     for (var i = 0; i < result.length; i++) {
+  //       console.log("여기");
+  //       var url = 'https://dapi.kakao.com/v2/local/search/keyword.json';
+  //       var queryParams = '?' + encodeURIComponent('page') + '=' + encodeURIComponent('1');
+  //       queryParams += '&' + encodeURIComponent('size') + '=' + encodeURIComponent('1');
+  //       queryParams += '&' + encodeURIComponent('query') + '=' + encodeURIComponent(result[i]);
+  // //console.log("여긴지나나");
+  //       request({
+  //         url: url + queryParams,
+  //         method: 'GET',
+  //         headers: {
+  //           'Authorization': 'KakaoAK fe7467539d84c3bb76f33d417a5399a4'
+  //         }
+  //       }, function(err, response, body) {
+  //         var js = JSON.parse(body);
+  //         var data = js['documents'];
+  //         var tt = [];
+  //         console.log(data[0]['place_name']);
+  //         console.log(data[0]['x']);
+  //         console.log(data[0]['y']);
+  //         tt.push(data[0]['place_name']);
+  //         tt.push(data[0]['x']);
+  //         tt.push(data[0]['y']);
+  //         total.push(tt);
+  //         console.log(total);
+  //       }); //request---end
+  //     }
+  //res.send(total)
 
 
-  }).catch(function(err) {
-    console.log(err);
-  });
+  // }).catch(function(err) {
+  //   console.log(err);
+  // });
 }); // router--end
 
 
