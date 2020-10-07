de0b2fae12b0ceeeb2f4637581b92f572b46e466
epikem <epikem1@gmail.com>
2020-10-07 14:43:05 +0900

    Make server as a node module - export express app for test - create index.js for run - update package.json

--- server/server.js
+++ server/server.js
@@ -43,6 +43,4 @@ app.use('/testlogin', testlogin);
 app.use('/noti', testnoti);
 app.use('/confirmed', confirm);
 
-app.listen(3000, function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+module.exports=app;
