99114af2168bde1bfb3b97c719bb3a663e80e8eb
epikem <epikem1@gmail.com>
2020-11-16 19:40:06 +0900

    Update deploy script

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -32,7 +32,6 @@ apt_wait () {
 apt_wait
 
 sudo apt install awscli -y >> $LOG 2>&1
-aws ssm get-parameters --region ap-northeast-2 --names Dahda-Environment-Variables --with-decryption --query Parameters[0].Value >> $ENV 2>&1
-source $ENV >> $LOG 2>&1
+aws ssm get-parameters --region ap-northeast-2 --names Dahda-Environment-Variables --with-decryption --query Parameters[0].Value >> $APPPATH/config 2>&1
 
 /bin/echo "$(date '+%Y-%m-%d %X'): ** Before Install Hook Completed **" >> $LOG 2>&1
