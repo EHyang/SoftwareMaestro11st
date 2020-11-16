328270f9641a3874e597aaff09e024cc9201ed62
epikem <epikem1@gmail.com>
2020-11-16 19:07:34 +0900

    Update deploy script - wait for dpkg lock release

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -15,6 +15,22 @@ ENV=$HOME/env
 
 # Do some actions before the installation
 
+apt_wait () {
+  while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  if [ -f /var/log/unattended-upgrades/unattended-upgrades.log ]; then
+    while sudo fuser /var/log/unattended-upgrades/unattended-upgrades.log >/dev/null 2>&1 ; do
+      sleep 1
+    done
+  fi
+}
+
+apt_wait
+
 sudo apt install awscli -y >> $LOG 2>&1
 aws ssm get-parameters --region ap-northeast-2 --names Dahda-Environment-Variables --with-decryption --query Parameters[0].Value >> $ENV 2>&1
 source $ENV >> $LOG 2>&1
